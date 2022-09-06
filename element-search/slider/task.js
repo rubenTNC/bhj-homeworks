6e3b02f782bc98c7c056821e088152bae9438049
rubenTNC <rubenen@mail.ru>
2022-09-06 19:24:28 +0300

    element-search

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,31 +1,43 @@
 const btnNext = document.querySelector(".slider__arrow_next");
 const btnPrev = document.querySelector(".slider__arrow_prev");
 const sliderItems = Array.from(document.querySelectorAll(".slider__item"));
+const sliderDots = Array.from(document.querySelectorAll(".slider__dot"));
 
 let count = 0;
 const amountЕlements = sliderItems.length;
 
+function render() {
+    sliderItems.forEach((element) => element.classList.remove("slider__item_active"));
+    sliderDots.forEach((element) => element.classList.remove("slider__dot_active"));
+    sliderItems[count].classList.add("slider__item_active");
+    sliderDots[count].classList.add("slider__dot_active");
+}
+
 btnNext.addEventListener("click", () => {
     if (count < amountЕlements - 1) {
         count++;
-        sliderItems[count - 1].classList.remove("slider__item_active");
-        sliderItems[count].classList.add("slider__item_active");
+        render();
     } else {
-        sliderItems[count].classList.remove("slider__item_active");
         count = 0;
-        sliderItems[count].classList.add("slider__item_active");
+        render();
+        
     }
 });
 
 btnPrev.addEventListener("click", () => {
     if (count > 0) {
         count--;
-        sliderItems[count + 1].classList.remove("slider__item_active");
-        sliderItems[count].classList.add("slider__item_active");
+        render();        
     }
     else {
-        sliderItems[count].classList.remove("slider__item_active");
         count = amountЕlements - 1;
-        sliderItems[count].classList.add("slider__item_active");
+        render();        
     }
 });
+
+sliderDots.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        count = sliderDots.indexOf(event.target);
+        render();
+    })
+})

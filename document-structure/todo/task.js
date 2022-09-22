991e317ef27745a48930549d04cc70e52f48ad2d
rubenTNC <rubenen@mail.ru>
2022-09-22 14:56:01 +0300

    document-structure

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,24 +2,27 @@ const tasksAdd = document.querySelector(".tasks__add");
 const tasksList = document.querySelector(".tasks__list");
 const tasksInput = document.querySelector(".tasks__input");
 
+
 tasksAdd.addEventListener("click", (event) => {
     event.preventDefault();
-    if (tasksInput.value)
-        tasksList.insertAdjacentHTML("afterbegin", `<div class="task">
-        <div class="task__title">
-            ${tasksInput.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-    </div>`);
-    tasksInput.value = "";
-    const tasksRemove = Array.from(document.querySelectorAll(".task__remove"));
-    tasksRemove.forEach(taskRemove => {
-        taskRemove.addEventListener("click", (event) => {
-            taskRemove.closest(".task").remove();
+    if (tasksInput.value.trim()) {
+        tasksList.insertAdjacentHTML("afterbegin",
+        `<div class="task">
+            <div class="task__title">
+                ${tasksInput.value}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>`);
+        tasksInput.value = "";
+        let task = document.querySelector(".task");
+        let taskRemove = task.querySelector(".task__remove");
+        taskRemove.addEventListener("click", () => {
+            task.remove()
         });
-    });
-
+    };
 });
+
+
 
 
 
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,9 +4,11 @@ hasTooltips.forEach(hasTooltip => {
     hasTooltip.addEventListener("click", (event) => {
         event.preventDefault();
         const tooltipsAactive = Array.from(document.querySelectorAll(".tooltip_active"));
-        tooltipsAactive.forEach(tooltipAactive => tooltipAactive.remove());
-        event.target.insertAdjacentHTML("afterEnd", `<div class="tooltip">${event.target.getAttribute("title")}</div>` );
+        tooltipsAactive.forEach(tooltipAactive => tooltipAactive.remove())
+        const height = event.target.offsetHeight
+        const {left, top} = event.target.getBoundingClientRect (event.target);
+        event.target.insertAdjacentHTML("afterEnd", `<div class="tooltip" style="left:${left}px; top:${top + height}px">${event.target.getAttribute("title")}</div>`);
         let tooltips = Array.from(document.querySelectorAll(".tooltip"));
-        tooltips.forEach(tooltip => tooltip.classList.toggle("tooltip_active"));
+        event.target.nextElementSibling.classList.toggle("tooltip_active")
     });
 })
